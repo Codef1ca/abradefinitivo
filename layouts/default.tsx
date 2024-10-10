4ce605d2f801a2569d60f98d0988f60d20bc5e06
codef1ca <ramospaula@gmail.com>
2024-10-10 11:08:34 -0300

    layout

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,13 +1,17 @@
+import { ReactNode } from "react";
 import { Head } from "./head";
 import { Navbar } from "@/components/navbar";
 
-export default function DefaultLayout({ children }) {
+interface DefaultLayoutProps {
+  children: ReactNode;
+}
+
+export default function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
     <div style={{ backgroundColor: '#FCF3EB' }}>
       <Head />
       <Navbar />
       <main>{children}</main>
-      
     </div>
   );
 }
